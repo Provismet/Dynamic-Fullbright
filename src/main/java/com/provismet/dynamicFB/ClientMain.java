b7e11b336abc427590712c90fe8efef7a46ef2d7
Provismet <17149901+Provismet@users.noreply.github.com>
2023-02-27 20:00:12 +0000

    Debug message is now translatable.

--- src/main/java/com/provismet/dynamicFB/ClientMain.java
+++ src/main/java/com/provismet/dynamicFB/ClientMain.java
@@ -5,7 +5,7 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
@@ -37,10 +37,8 @@ public class ClientMain implements ClientModInitializer {
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			while (toggleLighting.wasPressed()) {
 				LightingManager.toggleActive();
-				String mes = "";
-				if (LightingManager.isActive()) mes = "Dynamic Fullbright is on.";
-				else mes = "Dynamic Fullbright is off.";
-				client.player.sendMessage(Text.of(mes), true);	
+				String mes = LightingManager.isActive() ? "message.dynamicfullbright.on" : "message.dynamicfullbright.off";
+				client.player.sendMessage(new TranslatableText(mes), true);	
 			}
 		});
 	}
